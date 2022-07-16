e8b77546614ffbe258b6a7e881a92dea3c637412
Dan Poulsen <dangjogvara@gmail.com>
2022-07-16 22:25:14 +0100

    basic post-detail page

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 
 
-# Create your views here.
 def starting_page(request):
     return render(request, 'blog/index.html')
 
@@ -10,5 +9,5 @@ def posts(request):
     return render(request, 'blog/all-posts.html')
 
 
-def post_detail(request):
-    pass
+def post_detail(request, slug):
+    return render(request, 'blog/post-detail.html')
