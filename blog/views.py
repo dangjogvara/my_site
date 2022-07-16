6696e07350753ae29e9af6d773f3d14200ff3d6f
Dan Poulsen <dangjogvara@gmail.com>
2022-07-17 00:59:16 +0100

    display dynamic post-detail.html

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 from datetime import date
 from django.shortcuts import render
 
-init_posts = [
+all_posts = [
     {
         "slug": "hike-in-the-mountains",
         "image": "mountains.jpg",
@@ -75,7 +75,7 @@ def get_date(post):
 
 
 def starting_page(request):
-    sorted_posts = sorted(init_posts, key=get_date)
+    sorted_posts = sorted(all_posts, key=get_date)
     latest_posts = sorted_posts[-3:]
     return render(request, 'blog/index.html', {
         'posts': latest_posts
@@ -83,8 +83,13 @@ def starting_page(request):
 
 
 def posts(request):
-    return render(request, 'blog/all-posts.html')
+    return render(request, 'blog/all-posts.html', {
+        "all_posts": all_posts
+    })
 
 
 def post_detail(request, slug):
-    return render(request, 'blog/post-detail.html')
+    identified_post = next(post for post in all_posts if post['slug'] == slug)
+    return render(request, 'blog/post-detail.html', {
+        'post': identified_post
+    })
